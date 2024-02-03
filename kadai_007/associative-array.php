20927505f0057aa3bbf2575da70f13bcf14b8acf
yamada760 <153713243+yonasiro@users.noreply.github.com>
2024-02-04 06:29:21 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<! DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset=utf-8>
+        <title>>PHP基礎編</title>
+    </head>
+    <body>
+        <P>
+          <?PHP 
+          $personal_data=['name'=>'onion','price'=>'200','whigt'=>'160'];
+        print_r($personal_data);
+        ?>
+        </P>
+    </body>
+</html>

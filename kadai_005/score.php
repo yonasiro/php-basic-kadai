6779e03259163a5be682674f8add1de143c6e6f0
yamada760 <153713243+yonasiro@users.noreply.github.com>
2024-02-04 05:26:59 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,25 @@
+<! DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset=utf-8>
+        <title>>PHP基礎編</title>
+    </head>
+    <body>
+        <P>
+          <?PHP
+          $score1=80;
+          $score2=60;
+          $score3=55;
+          $score4=40;
+          $score5=100;
+          $score6=25;
+          $score7=80;
+          $score8=95;
+          $score9=30;
+          $score10=60;
+          echo ($score1+$score2+$score3+$score4+$score5+
+          $score6+$score7+$score8+$score9+$score10)/10;
+          ?>
+        </P>
+    </body>
+</html>
